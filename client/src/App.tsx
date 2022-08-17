004e73befdde0dffd529503c8efd35a45f252574
minsu-zip <alstn4814@gmail.com>
2022-08-17 17:35:31 +0900

    style : global style 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { theme } from '@constants/theme';
 import { ThemeProvider } from '@mui/material/styles';
+import GlobalStyle from '@src/GlobalStyles';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <div className='App'></div>
     </ThemeProvider>
   );
---
+++ client/src/GlobalStyles.tsx
@@ -0,0 +1,52 @@
+import { Global, css } from '@emotion/react';
+
+const GlobalStyle = () => (
+  <Global
+    styles={css`
+      @import url(‘https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap’);
+      body,
+      #root {
+        width: 100%;
+        height: 100%;
+      }
+      body {
+        margin: 0;
+        font-size: 14px;
+        font-family: 'Noto Sans KR';
+        user-select: none;
+        font-weight: normal;
+        font-style: normal;
+      }
+      h1,
+      h2,
+      h3,
+      h4,
+      h5,
+      h6 {
+        margin-top: 0;
+      }
+      a {
+        color: inherit;
+        text-decoration: none;
+        outline: none;
+      }
+      input,
+      textarea,
+      button {
+        border: 0;
+        padding: 0;
+        appearance: none;
+        -moz-appearance: none;
+        -webkit-appearance: none;
+        border-radius: 0;
+        -webkit-border-radius: 0;
+        -moz-border-radius: 0;
+      }
+      * {
+        box-sizing: border-box;
+      }
+    `}
+  />
+);
+
+export default GlobalStyle;
