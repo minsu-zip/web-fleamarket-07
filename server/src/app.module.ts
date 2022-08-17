770b689d45d59ded56a46e36f684eab100832cb9
minsu-zip <alstn4814@gmail.com>
2022-08-18 00:13:52 +0900

    feat : mysql service 등록

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config'
 import { validateEnv } from './config/env.validation';
+import { MySqlConfigModule } from './config/database/config.module'
+import { MySqlConfigService } from './config/database/config.service'
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -10,7 +13,12 @@ import { validateEnv } from './config/env.validation';
       envFilePath: '.env',
       isGlobal: true,
       validate: validateEnv,
-    })
+    }),
+      TypeOrmModule.forRootAsync({
+      imports: [MySqlConfigModule],
+      useClass: MySqlConfigService,
+      inject: [MySqlConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
