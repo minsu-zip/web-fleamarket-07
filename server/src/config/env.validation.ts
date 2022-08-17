7308af8fe635109941cf937996107873ff89c1e1
minsu-zip <alstn4814@gmail.com>
2022-08-17 23:58:57 +0900

    feat : 환경변수 타입 체크

---
+++ server/src/config/env.validation.ts
@@ -0,0 +1,47 @@
+import { plainToInstance } from "class-transformer";
+import { IsEnum, IsNumber, IsString, validateSync } from "class-validator";
+
+
+export enum Environment {
+  Development = "development",
+  Production = "production",
+  Test = "test",
+  Provision = "provision",
+}
+
+class EnvironmentVariables {
+  @IsNumber()
+  PORT = 5000;
+
+  @IsEnum(Environment)
+  NODE_ENV: Environment = Environment.Production;
+
+  @IsString()
+  DB_HOST: string;
+
+  @IsString()
+  DB_NAME: string;
+
+  @IsString()
+  DB_USER: string;
+
+  @IsString()
+  DB_PASS: string;
+
+  @IsNumber()
+  DB_PORT = 3306;
+}
+
+export function validateEnv(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+
+  const errors = validateSync(validatedConfig, { skipMissingProperties: false });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return validatedConfig;
+}
