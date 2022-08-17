bfc4afe4e2567b610c6468b71271f307f657f466
minsu-zip <alstn4814@gmail.com>
2022-08-17 23:59:56 +0900

    feat : 환경변수 파일 전역 설정 및 타입 체크

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config'
+import { validateEnv } from './config/env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+     ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      validate: validateEnv,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
