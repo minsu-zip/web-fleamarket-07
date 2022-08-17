17777690e514a3d71fb06cc2e67789235056a2a2
minsu-zip <alstn4814@gmail.com>
2022-08-17 17:53:43 +0900

    fix : react-query 전역 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
 
 import { theme } from '@constants/theme';
 import { ThemeProvider } from '@mui/material/styles';
 import GlobalStyle from '@src/GlobalStyles';
 
+const queryClient = new QueryClient();
+
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <RecoilRoot>
-        <div className='App'></div>
-      </RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <div className='App'></div>
+        </RecoilRoot>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 };
