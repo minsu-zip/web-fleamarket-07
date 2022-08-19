b2a42fea593c0e9e4dafe25eccad7a7a53d51a68
pyo-sh <pygosky@gmail.com>
2022-08-19 23:00:14 +0900

    feat : OAuth 통신 시 서버에서 token, user 정보를 사용하는 로직 작성

--- server/src/domain/user/user.module.ts
+++ server/src/domain/user/user.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User } from './entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
+import { LocationModule } from '../location/location.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), ConfigModule, LocationModule],
   controllers: [UserController],
   providers: [UserService],
 })
--- server/src/domain/user/user.service.ts
+++ server/src/domain/user/user.service.ts
@@ -1,13 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import axios, { AxiosResponse } from 'axios';
+import { LocationService } from '../location/location.service';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    private readonly configService: ConfigService,
+    private readonly locationService: LocationService,
   ) {
     this.userRepository = userRepository;
   }
@@ -31,5 +36,54 @@ export class UserService {
 
     await this.userRepository.update(id, updateUserDto);
     return { ...pureUser, ...updateUserDto };
+  }
+
+  async verifyUser(name: string): Promise<User> {
+    let user = await this.userRepository.findOneBy({ name });
+
+    if (!user) {
+      const location = await this.locationService.getDefault();
+      const newUser = await this.userRepository.create({
+        name,
+        location1: location,
+      });
+      user = await this.userRepository.save(newUser);
+    }
+
+    return user;
+  }
+
+  async attachGithubId(code: string): Promise<string> {
+    const accessUrl = 'https://github.com/login/oauth/access_token';
+    const accessBody = {
+      code,
+      client_id: this.configService.get<string>('GITHUB_ID'),
+      client_secret: this.configService.get<string>('GITHUB_SECRET'),
+    };
+    const accessOptions = {
+      headers: { accept: 'application/json' },
+    };
+    const { data: access_data }: AxiosResponse = await axios.post(
+      accessUrl,
+      accessBody,
+      accessOptions,
+    );
+
+    if (access_data.error) {
+      throw new HttpException(
+        { status: HttpStatus.UNAUTHORIZED, message: '깃허브 인증 실패' },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    const { access_token } = access_data;
+    const userUrl = 'https://api.github.com/user';
+    const { data: userData }: AxiosResponse = await axios.get(userUrl, {
+      headers: { Authorization: `token ${access_token}` },
+    });
+
+    const { login }: { login: string } = userData;
+
+    return login;
   }
 }
