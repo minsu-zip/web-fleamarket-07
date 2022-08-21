c3f43c21b8f8c3a3aa864f061164966adbdd9047
pyo-sh <pygosky@gmail.com>
2022-08-21 15:20:25 +0900

    refactor : React의 설정을 index.tsx로 이동

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { RecoilRoot } from 'recoil';
-import { theme } from '@constants/theme';
-import { ThemeProvider } from '@mui/material/styles';
-import GlobalStyle from '@src/GlobalStyles';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import {
   Main,
   Category,
@@ -16,29 +11,18 @@ import {
   NotFound,
 } from '@src/pages';
 
-const queryClient = new QueryClient();
-
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <QueryClientProvider client={queryClient}>
-        <RecoilRoot>
-          <BrowserRouter>
-            <Routes>
-              <Route path='*' element={<NotFound />} />
-              <Route path='/' element={<Main />} />
-              <Route path='/category' element={<Category />} />
-              <Route path='/locationEdit' element={<LocationEdit />} />
-              <Route path='/newProduct' element={<NewProduct />} />
-              <Route path='/productDetail' element={<ProductDetail />} />
-              <Route path='/signUp' element={<SignUp />} />
-              <Route path='/signIn' element={<SignIn />} />
-            </Routes>
-          </BrowserRouter>
-        </RecoilRoot>
-      </QueryClientProvider>
-    </ThemeProvider>
+    <Routes>
+      <Route path='*' element={<NotFound />} />
+      <Route path='/' element={<Main />} />
+      <Route path='/category' element={<Category />} />
+      <Route path='/locationEdit' element={<LocationEdit />} />
+      <Route path='/newProduct' element={<NewProduct />} />
+      <Route path='/productDetail' element={<ProductDetail />} />
+      <Route path='/signUp' element={<SignUp />} />
+      <Route path='/signIn' element={<SignIn />} />
+    </Routes>
   );
 };
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,32 @@
-import App from '@src/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from '@mui/material/styles';
+import GlobalStyle from '@src/GlobalStyles';
+import App from '@src/App';
+import { theme } from '@constants/theme';
 
 import reportWebVitals from './reportWebVitals';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </RecoilRoot>
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 );
 
