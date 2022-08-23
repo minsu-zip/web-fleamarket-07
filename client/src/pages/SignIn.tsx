fcbc113d6245ccdd16326940a154d1c3305abdb7
minsu-zip <alstn4814@gmail.com>
2022-08-24 01:14:04 +0900

    feat : 로그인 여부에 따른 컴포넌트 분기 처리

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -2,32 +2,40 @@ import TopBar from '@components/molecules/TopBar';
 import styled from '@emotion/styled';
 import Button from '@mui/material/Button';
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { authAtom } from '@stores/AuthRecoil';
+
 import { COLOR } from '@constants/style';
 import { GITHUB_ID } from '@constants/envs';
+import LogOut from '@components/molecules/LogOut';
 
 const SignIn: React.FC = () => {
+  const Auth = useRecoilValue(authAtom);
+
+  if (Auth?.id) {
+    return <LogOut auth={Auth} />;
+  }
+
   return (
     <>
       <TopBar title='로그인' />
       <div style={{ marginTop: '40px' }}>
         <Img src={'baemin.png'} />
 
-        <>
-          <SocialButton variant='contained'>
-            <a
-              href={`https://github.com/login/oauth/authorize?client_id=${GITHUB_ID}`}
-            >
-              <GithubWrapperDiv>
-                <div>
-                  <img className='github' src={'github.svg'} />
-                </div>
-                <div style={{ paddingLeft: '20px' }}>
-                  <span>깃허브로 계속하기</span>
-                </div>
-              </GithubWrapperDiv>
-            </a>
-          </SocialButton>
-        </>
+        <SocialButton variant='contained'>
+          <a
+            href={`https://github.com/login/oauth/authorize?client_id=${GITHUB_ID}`}
+          >
+            <GithubWrapperDiv>
+              <div>
+                <img className='github' src={'github.svg'} alt='githubIcon' />
+              </div>
+              <div style={{ paddingLeft: '20px' }}>
+                <span>깃허브로 계속하기</span>
+              </div>
+            </GithubWrapperDiv>
+          </a>
+        </SocialButton>
       </div>
     </>
   );
