69d7ad5f875232440dbc120ab0aee4da63d2a74f
minsu-zip <alstn4814@gmail.com>
2022-08-24 01:10:18 +0900

    feat : 로그인 판별 및 라우팅 로직 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,8 +12,12 @@ import {
   Menu,
 } from '@src/pages';
 import Animator from '@components/Animator';
+import useAuth from '@hooks/useAuth';
+import Verification from '@components/Verification';
 
 const App: React.FC = () => {
+  useAuth();
+
   return (
     <Animator>
       <Routes>
@@ -21,11 +25,13 @@ const App: React.FC = () => {
         <Route path='/' element={<Main />} />
         <Route path='/menu' element={<Menu />} />
         <Route path='/category' element={<Category />} />
-        <Route path='/locationEdit' element={<LocationEdit />} />
-        <Route path='/newProduct' element={<NewProduct />} />
         <Route path='/productDetail' element={<ProductDetail />} />
         <Route path='/signUp' element={<SignUp />} />
         <Route path='/signIn' element={<SignIn />} />
+        <Route path='' element={<Verification />}>
+          <Route path='/locationEdit' element={<LocationEdit />} />
+          <Route path='/newProduct' element={<NewProduct />} />
+        </Route>
       </Routes>
     </Animator>
   );
