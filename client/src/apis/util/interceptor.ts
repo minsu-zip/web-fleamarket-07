09e5c78b3328e9c2a4e1f084f6f0c46ab62d1dd3
minsu-zip <alstn4814@gmail.com>
2022-08-23 16:49:27 +0900

    feat : axios 인터셉터 정의
    사용자 인증이 필요한 필요한 api 요청일 경우 인터셉터에서 토큰값이 있으면 헤더에 포함해서 전송

---
+++ client/src/apis/util/interceptor.ts
@@ -0,0 +1,21 @@
+import { AxiosError, AxiosInstance, AxiosRequestConfig } from 'axios';
+import { getCookie } from '@utils/cookie';
+
+const setInterceptor = (instance: AxiosInstance) => {
+  instance.interceptors.request.use(
+    (config: AxiosRequestConfig) => {
+      const authToken = getCookie('auth');
+
+      if (authToken) {
+        config.headers = {
+          authorization: authToken ? authToken : '',
+        };
+      }
+
+      return config;
+    },
+    (error: AxiosError) => Promise.reject(error.response),
+  );
+};
+
+export { setInterceptor };
