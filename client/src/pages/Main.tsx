8bd28cd532d2c1a74e6864440cef8be3d96aa3ea
pyo-sh <pygosky@gmail.com>
2022-08-23 20:35:55 +0900

    feat : Product All Api 를 통해서 데이터를 받고 렌더링하는 작업

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,176 +1,63 @@
-import MenuHeader from '@components/organisms/MenuHeader';
-import ProductItem from '@components/organisms/ProductItem';
 import React from 'react';
+import { useQuery } from 'react-query';
 import styled from '@emotion/styled';
-import { Fab } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import { useNavigate } from 'react-router-dom';
+import MainLayout from '@components/organisms/MainLayout';
+import ProductItem from '@components/organisms/ProductItem';
+import Guide from '@components/atoms/Guide';
 import { COLOR } from '@constants/style';
-
-const productList = [
-  {
-    id: 1,
-    title: '도자기',
-    price: 15555,
-    titleImage: undefined,
-    likes: 3,
-    isLike: true,
-    chats: 2,
-    createdAt: undefined,
-    updatedAt: undefined,
-    deletedAt: undefined,
-    locationId: 1,
-    locationName: '역삼동',
-    userId: 0,
-    userName: '',
-  },
-  {
-    id: 2,
-    title: '도자기123312',
-    price: 0,
-    titleImage: undefined,
-    likes: 0,
-    isLike: true,
-    chats: 0,
-    createdAt: undefined,
-    updatedAt: undefined,
-    deletedAt: undefined,
-    locationId: 1,
-    locationName: '역삼동',
-    userId: 0,
-    userName: '',
-  },
-  {
-    id: 3,
-    title: '도자기asdsad',
-    price: 123123,
-    titleImage: undefined,
-    likes: 3,
-    isLike: false,
-    chats: 2,
-    createdAt: undefined,
-    updatedAt: undefined,
-    deletedAt: undefined,
-    locationId: 1,
-    locationName: '역삼동',
-    userId: 0,
-    userName: '',
-  },
-  {
-    id: 3,
-    title: '도자기asdsad',
-    price: 123123,
-    titleImage: undefined,
-    likes: 3,
-    isLike: false,
-    chats: 2,
-    createdAt: undefined,
-    updatedAt: undefined,
-    deletedAt: undefined,
-    locationId: 1,
-    locationName: '역삼동',
-    userId: 0,
-    userName: '',
-  },
-  {
-    id: 3,
-    title: '도자기asdsad',
-    price: 123123,
-    titleImage: undefined,
-    likes: 3,
-    isLike: false,
-    chats: 2,
-    createdAt: undefined,
-    updatedAt: undefined,
-    deletedAt: undefined,
-    locationId: 1,
-    locationName: '역삼동',
-    userId: 0,
-    userName: '',
-  },
-  {
-    id: 3,
-    title: '도자기asdsad',
-    price: 123123,
-    titleImage: undefined,
-    likes: 3,
-    isLike: false,
-    chats: 2,
-    createdAt: undefined,
-    updatedAt: undefined,
-    deletedAt: undefined,
-    locationId: 1,
-    locationName: '역삼동',
-    userId: 0,
-    userName: '',
-  },
-  {
-    id: 3,
-    title: '도자기asdsad',
-    price: 123123,
-    titleImage: undefined,
-    likes: 3,
-    isLike: false,
-    chats: 2,
-    createdAt: undefined,
-    updatedAt: undefined,
-    deletedAt: undefined,
-    locationId: 1,
-    locationName: '역삼동',
-    userId: 0,
-    userName: '',
-  },
-  {
-    id: 3,
-    title: '도자기asdsad',
-    price: 123123,
-    titleImage: undefined,
-    likes: 3,
-    isLike: false,
-    chats: 2,
-    createdAt: undefined,
-    updatedAt: undefined,
-    deletedAt: undefined,
-    locationId: 1,
-    locationName: '역삼동',
-    userId: 0,
-    userName: '',
-  },
-];
+import type { TProductSummary } from '@fleamarket/common';
+import { getProductAllAPI } from '@apis/product';
 
 const Main: React.FC = () => {
-  const navigate = useNavigate();
+  // 1. TODO : Location ID 를 동적으로 관리하기
+  // 2. TODO : Category ID 를 넣어줄 수 있기
+  const {
+    isLoading,
+    isError,
+    data: productList,
+  } = useQuery<TProductSummary[]>(
+    'products',
+    () => getProductAllAPI({ locationId: 1 }),
+    {
+      refetchOnWindowFocus: false,
+      retry: 0,
+    },
+  );
+
+  if (isError)
+    return (
+      <MainLayout backgroundColor={COLOR.background}>
+        <Guide.Error />
+      </MainLayout>
+    );
+
+  if (isLoading)
+    return (
+      <MainLayout>
+        <Guide.Loading />
+      </MainLayout>
+    );
+
+  if (!productList || productList.length === 0)
+    return (
+      <MainLayout backgroundColor={COLOR.background}>
+        <Guide.Empty />
+      </MainLayout>
+    );
 
   return (
-    <ContainerDiv>
-      <MenuHeader />
-      <ItemWrapperDiv id='item'>
-        {productList.map((item) => (
-          <ProductItem product={item} />
+    <MainLayout>
+      <ContentWrapperDiv id='item'>
+        {productList.map((item, i) => (
+          <ProductItem key={i} product={item} />
         ))}
-      </ItemWrapperDiv>
-      <FabWrapper color='primary' onClick={() => navigate('newPRoduct')}>
-        <AddIcon />
-      </FabWrapper>
-    </ContainerDiv>
+      </ContentWrapperDiv>
+      <MainLayout.FAB />
+    </MainLayout>
   );
 };
 
-const ContainerDiv = styled.div`
-  width: 100%;
-  height: 100%;
-  position: relative;
-
-  display: flex;
-  flex-direction: column;
-  overflow: none;
-
-  &:hover > #item::-webkit-scrollbar-thumb {
-    background-color: ${COLOR.title};
-  }
-`;
-
-const ItemWrapperDiv = styled.div`
+const ContentWrapperDiv = styled.div`
   flex: 1;
   overflow-y: scroll;
 
@@ -186,11 +73,5 @@ const ItemWrapperDiv = styled.div`
     border-radius: 10px;
   }
 `;
-
-const FabWrapper = styled(Fab)({
-  position: 'absolute',
-  right: '1rem',
-  bottom: '1rem',
-});
 
 export default Main;
