3f02280a509e5ff1f32c3682a8fdd210242d1f14
minsu-zip <alstn4814@gmail.com>
2022-08-25 01:54:54 +0900

    feat : 판매 리스트 컴포넌트

---
+++ client/src/components/organisms/SaleList.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { userSaleListAPI } from '@apis/product';
+import type { TProductSummary } from '@fleamarket/common';
+import ProductItem from './ProductItem';
+import { useQuery } from 'react-query';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Dropdown from '@components/molecules/Dropdown';
+
+const dropDownList = ['수정하기', '삭제하기'];
+
+const SaleList = () => {
+  const handleClick = (value: string) => {
+    // 함수 로직 작성 나중에 실제 데이터 이용시 수정
+    if (value === '수정하기') {
+      //
+    } else {
+      //
+    }
+  };
+
+  const { data: userSaleList } = useQuery<TProductSummary[] | undefined>(
+    '',
+    () => userSaleListAPI(1),
+    {
+      refetchOnWindowFocus: false,
+      retry: 0,
+    },
+  );
+
+  return (
+    <>
+      {userSaleList?.map((product) => (
+        <ProductItem key={product.id} product={product}>
+          <Dropdown dropDownList={dropDownList} handleClick={handleClick}>
+            <MoreVertIcon />
+          </Dropdown>
+        </ProductItem>
+      ))}
+    </>
+  );
+};
+
+export default SaleList;
