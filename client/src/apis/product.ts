9cb83ef8b11dcf0b1f2a45826d22271e67a1eb37
minsu-zip <alstn4814@gmail.com>
2022-08-25 01:53:26 +0900

    feat : 유저 판매정보 API 로직

--- client/src/apis/product.ts
+++ client/src/apis/product.ts
@@ -1,10 +1,10 @@
 import { axiosAuth } from './util';
-import { TProductAllQuery } from '@fleamarket/common';
+import { TProductAllQuery, TProductSummary } from '@fleamarket/common';
 
 export const getProductAllAPI = async ({
   locationId,
   categoryId,
-}: TProductAllQuery) => {
+}: TProductAllQuery): Promise<TProductSummary[] | undefined> => {
   const locationQuery = `locationId=${locationId}`;
   const categoryQuery = categoryId ? `&categoryId=${categoryId}` : '';
 
@@ -18,4 +18,23 @@ export const getProductAllAPI = async ({
   const { products } = response.data;
 
   return products;
+};
+
+export const userSaleListAPI = async (
+  userId: number,
+): Promise<TProductSummary[] | undefined> => {
+  try {
+    const response = await axiosAuth.get(`product/saleList/${userId}`);
+    const status = Math.floor(response.status / 100) * 100;
+
+    if (status === 200) {
+      const { userSaleList } = response.data;
+
+      return userSaleList;
+    } else {
+      throw new Error('잘못된 응답값입니다.');
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
