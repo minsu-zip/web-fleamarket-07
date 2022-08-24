9f52bffddb41a72b25681d6be4fe30e0958e0d42
pyo-sh <pygosky@gmail.com>
2022-08-25 04:53:04 +0900

    feat : Chat 타입 지정

--- common/index.ts
+++ common/index.ts
@@ -2,4 +2,6 @@ export * from './types/product';
 export * from './types/TCategory';
 export * from './types/TImage';
 export * from './types/user';
+export * from './types/TRoom';
+export * from './types/TChat';
 export * from './constants/events';
