bd9ec9b462207ffb0d9c5775b08800d2efcec818
minsu-zip <alstn4814@gmail.com>
2022-08-24 10:55:41 +0900

    fix : 함수명 변경(코드리뷰 반영)

--- client/src/components/Verification.tsx
+++ client/src/components/Verification.tsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { authAtom } from '@stores/AuthRecoil';
 
-export default function Unauthorized() {
+export default function Verification() {
   const Auth = useRecoilValue(authAtom);
   if (Auth) return <Outlet />;
 
