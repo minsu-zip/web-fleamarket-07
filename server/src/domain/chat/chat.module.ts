72af2926262a358fad33d8b33f45d6a7ba02bf01
pyo-sh <pygosky@gmail.com>
2022-08-25 13:53:00 +0900

    feat : Server에서 Socket 설정 및 env 추가

--- server/src/domain/chat/chat.module.ts
+++ server/src/domain/chat/chat.module.ts
@@ -3,11 +3,12 @@ import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Chat } from './entities/chat.entity';
+import { EventsGateway } from './chat.gateway';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Chat])],
   controllers: [ChatController],
-  providers: [ChatService],
-  exports: [ChatService],
+  providers: [ChatService, EventsGateway],
+  exports: [ChatService, EventsGateway],
 })
 export class ChatModule {}
