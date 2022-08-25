135c749eb03790fce085603ccaf1dc4f26b11e77
pyo-sh <pygosky@gmail.com>
2022-08-25 13:55:10 +0900

    bug : Common에서 Chat Type Build를 못하던 버그 수정

--- common/index.ts
+++ common/index.ts
@@ -1,7 +1,7 @@
 export * from './types/product';
 export * from './types/user';
+export * from './types/chat';
 export { default as TCategory } from './types/TCategory';
 export { default as TImage } from './types/TImage';
 export { default as TRoom } from './types/TRoom';
-export { default as TChat } from './types/TChat';
 export * from './constants/events';
