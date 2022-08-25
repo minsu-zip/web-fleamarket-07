b291d4d3dd0fe4869a1bb7f256ad5c252677904d
minsu-zip <alstn4814@gmail.com>
2022-08-25 14:24:46 +0900

    refactor : 사용자 로그인 처리 로직 변경

--- client/src/stores/AuthRecoil.ts
+++ client/src/stores/AuthRecoil.ts
@@ -1,9 +1,14 @@
 import { atom } from 'recoil';
 import { TUserGithub } from '@fleamarket/common';
+import { getCookie } from '@utils/cookie';
+import jwt_decode from 'jwt-decode';
+
+const authToken = getCookie('auth');
+const decoded: TUserGithub | null = authToken ? jwt_decode(authToken) : null;
 
 const authAtom = atom<TUserGithub | null>({
   key: 'authAtom',
-  default: null,
+  default: decoded,
 });
 
 export { authAtom };
