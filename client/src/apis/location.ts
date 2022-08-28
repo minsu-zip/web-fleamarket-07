319a8cd8c5e27d9341fee52a84114f4a328b3a1c
pyo-sh <pygosky@gmail.com>
2022-08-29 03:20:42 +0900

    feat : delete location API front

--- client/src/apis/location.ts
+++ client/src/apis/location.ts
@@ -1,10 +1,28 @@
 import { axiosAuth } from './util';
-import { TLocation, TLocationCreate } from '@fleamarket/common';
+import {
+  TLocation,
+  TLocationCreate,
+  TLocationDelete,
+} from '@fleamarket/common';
 
 export const createLocationAPI = async ({
   region,
 }: TLocationCreate): Promise<TLocation[]> => {
   const response = await axiosAuth.post(`location`, { region });
+
+  const status = Math.floor(response.status / 100) * 100;
+
+  if (status !== 200) throw response;
+
+  const { locations } = response.data;
+
+  return locations;
+};
+
+export const deleteLocationAPI = async (
+  locationDeleteDto: TLocationDelete,
+): Promise<TLocation[]> => {
+  const response = await axiosAuth.post(`location`, locationDeleteDto);
 
   const status = Math.floor(response.status / 100) * 100;
 
