c222dfbfdc43fe718b8edfef392318fcd1f0a3b5
minsu-zip <alstn4814@gmail.com>
2022-08-28 22:13:14 +0900

    feat : 사용자가 포함하고 있는 위치 정보 api

--- client/src/apis/user.ts
+++ client/src/apis/user.ts
@@ -28,3 +28,20 @@ export const logoutAPI = async () => {
     console.error('로그아웃 실패', error);
   }
 };
+
+export const getUserLocationAPI = async (userId: number | undefined) => {
+  try {
+    if (!userId) return;
+    const userLocationList = await axiosAuth.get(`user/userLocation/${userId}`);
+
+    const status = Math.floor(userLocationList.status / 100) * 100;
+
+    if (status === 200) {
+      return userLocationList.data;
+    } else {
+      throw new Error('잘못된 응답값입니다.');
+    }
+  } catch (error) {
+    console.log('사용자 정보를 파악할 수 없습니다.', error);
+  }
+};
